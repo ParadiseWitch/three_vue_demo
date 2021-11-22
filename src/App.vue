918dc0fa3f8abad0e8cedf5ad6d883b3b01d9c4f
Maiiiiiid <zhp92@outlook.com>
2021-11-22 10:12:32 +0800

    storybook 对接tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,6 @@
 <template>
   <CalendarGraph></CalendarGraph>
 
-   <div
-    class="
-      p-6
-      max-w-sm
-      mx-auto
-      bg-white
-      rounded-xl
-      shadow-md
-      flex
-      items-center
-      space-x-4
-    "
-  >
-    <div class="flex-shrink-0">
-      <img class="h-12 w-12" src="./img/logo.ico" alt="ChitChat Logo" />
-    </div>
-    <div>
-      <div class="text-xl font-medium text-black">ChitChat</div>
-      <p class="text-gray-500">You have a new message!</p>
-    </div>
-  </div>
-
-  <div class="bg-white dark:bg-gray-800">
-    <h1 class="text-gray-900 dark:text-white">Dark mode is here!</h1>
-    <p class="text-gray-600 dark:text-gray-300">Lorem ipsum...</p>
-  </div>
 </template>
 
 <script>
